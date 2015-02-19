3908d3e72854dac01261535903afb0845f424939
AndrewDominick <andrew.dominick@yahoo.com>
2015-02-19 14:28:10 -0600

    Reinstalled correct file upload node

---
+++ js/fileupload.js
@@ -0,0 +1,13 @@
+require('blueimp-file-upload');
+
+// Basic file upload capability
+$(function () {
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo(document.body);
+            });
+        }
+    });
+});

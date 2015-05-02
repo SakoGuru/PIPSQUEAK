13a158206fa7c8b0fcad01604d7a885e06098bbd
wcmoors <bill.moors@gmail.com>
2015-05-02 02:12:05 -0500

    fixed random enter num secs error

--- js/PIPSQUEAK.js
+++ js/PIPSQUEAK.js
@@ -172,6 +172,7 @@ $(document).ready(function(global){
 	$(".videoSizer").height(codesize);
 
 	$('#durationModal').on('shown.bs.modal', function () {
+		$("#error").html("");
 		$('#dur').focus();
 	})
 
